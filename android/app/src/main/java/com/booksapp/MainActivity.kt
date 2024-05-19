d9fef586232ce995198563bad1fe6378262a5d46
Punith M <punithm300@gmail.com>
2024-05-19 22:29:02 +0530

    packges and react navigation added

--- android/app/src/main/java/com/booksapp/MainActivity.kt
+++ android/app/src/main/java/com/booksapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.booksapp
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -6,13 +7,15 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "BooksApp"
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+      }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,40 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["@react-native/babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+            ".android.js",
+            ".android.tsx",
+            ".ios.js",
+            ".ios.tsx",
+          ],
+          root: ["."],
+          alias: {
+            "@assets": "./app/assets",
+            "@components": "./app/components",
+            "@screens": "./app/screens",
+            "@configs": "./app/configs",
+            "@routes": "./app/routes",
+            "@features": "./app/features",
+            "@services": "./app/services",
+          },
+        },
+      ],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
+  };
 };
