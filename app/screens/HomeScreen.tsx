0ab2190bad84cae92530817ae0c237cc342b2f9e
Punith M <punithm300@gmail.com>
2024-05-20 18:28:43 +0530

    Firebase Auth logic added

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,16 +1,43 @@
-import React, { FC, useEffect } from "react";
-import { LogBox } from "react-native";
-import SearchBar from "../components/SearchBar";
-import ParallaxScrollView from "react-native-parallax-scroll-view";
-import HeaderComp from "../components/HeaderComp";
-import { BookViewProps } from "@configs/Types";
-import BooksView from "@components/BooksView";
+import React, {FC, useEffect} from 'react';
+import {
+  BackHandler,
+  Button,
+  LogBox,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Alert,
+} from 'react-native';
+import SearchBar from '../components/SearchBar';
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import HeaderComp from '../components/HeaderComp';
+import {BookViewProps} from '@configs/Types';
+import BooksView from '@components/BooksView';
+import auth from '@react-native-firebase/auth';
 
-const HomeScreen: FC<BookViewProps> = ({ navigation }) => {
+const HomeScreen: FC<BookViewProps> = ({navigation}) => {
   useEffect(() => {
-    LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
+    LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => true,
+    );
+    return () => backHandler.remove();
   }, []);
 
+  const logoutHandler = () => {
+      auth()
+        .signOut()
+        .then(() => {
+          console.log('User has been logged out');
+          Alert.alert('You have been logged out');
+          navigation.navigate('Auth');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  };
   return (
     <ParallaxScrollView
       parallaxHeaderHeight={250}
@@ -18,11 +45,25 @@ const HomeScreen: FC<BookViewProps> = ({ navigation }) => {
       renderForeground={() => <SearchBar />}
       stickyHeaderHeight={80}
       renderStickyHeader={() => <HeaderComp height={80} />}
-      backgroundSpeed={2}
-    >
+      backgroundSpeed={2}>
+      <TouchableOpacity onPress={logoutHandler} style={styles.button}>
+        <Text style={styles.btnTxt}>Logout</Text>
+      </TouchableOpacity>
       <BooksView navigation={navigation} />
     </ParallaxScrollView>
   );
 };
+const styles = StyleSheet.create({
+  button: {
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'skyblue',
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginTop: 30,
+  },
+  btnTxt: {color: 'white', paddingHorizontal: 8, paddingVertical: 12}
 
+});
 export default HomeScreen;
--- app/screens/authentication/SignInScreen/SignInScreen.tsx
+++ app/screens/authentication/SignInScreen/SignInScreen.tsx
@@ -1,20 +1,66 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableOpacity, Alert} from 'react-native';
+import React, {useState} from 'react';
 import {styles} from './styles';
 import CircleGeoShape from '@assets/shared/circle-geo-shape.svg';
 import XGeoShape from '@assets/shared/X-geo-shape.svg';
 import SideGeoShapes from '@assets/shared/side-geo-shapes.svg';
 import {TextInput} from 'react-native-gesture-handler';
+import auth from '@react-native-firebase/auth';
+
 type Props = {};
 
 const SignInScreen = (props: Props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState('');
+
+  const signUpHandler = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+        Alert.alert('User account created & signed in!');
+        props?.navigation.navigate('Main');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+          Alert.alert('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+          Alert.alert('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+  const signingInHandler = () => {
+    
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+        Alert.alert('signed in!');
+        props?.navigation.navigate('Main');
+      })
+      .catch(error => {
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+          Alert.alert('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
   return (
     <View style={styles.container}>
       <CircleGeoShape />
       <XGeoShape style={styles.XGeoShape} />
       <View style={styles.appTitleView}>
         <TextInput
-          // onChangeText={e => handleInput(e)}
+          onChangeText={e => setEmail(e)}
           style={styles.inputStyle}
           placeholder="Enter you mail ID"
           placeholderTextColor="#0006"
@@ -22,16 +68,17 @@ const SignInScreen = (props: Props) => {
       </View>
       <View style={styles.appTitleView}>
         <TextInput
-          // onChangeText={e => handleInput(e)}
+          onChangeText={e => setPassword(e)}
           style={styles.inputStyle}
           placeholder="Enter your Password"
           placeholderTextColor="#0006"
         />
       </View>
-      <TouchableOpacity
-        onPress={() => props?.navigation.navigate('Main')}
-        style={styles.button}>
-        <Text style={styles.btnTxt}>Submit</Text>
+      <TouchableOpacity onPress={signUpHandler} style={styles.button}>
+        <Text style={styles.btnTxt}>SignUp</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={signingInHandler} style={styles.button}>
+        <Text style={styles.btnTxt}>Login</Text>
       </TouchableOpacity>
       <SideGeoShapes style={styles.sideGeoShapes} />
     </View>
